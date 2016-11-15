09131fcedd038cdeeac7b066dc21087d34be0960
Anish <anish.s.shekhawat@gmail.com>
2016-11-15 01:46:52 -0800

    Changed point struct & added Euclidean dist. func.

--- k-means.py
+++ k-means.py
@@ -1,22 +1,37 @@
+
+def euclidean_dist(pointA, pointB):
+    """Calculates Euclidean distance between two points and returns float"""
+
+    sum = 0.0
+    for a, b in zip(pointA, pointB):
+        diff = a-b
+        diff = diff ** 2
+        sum = sum + diff
+
+    return sum ** 0.5
+
+
 plist = []
 with open("data.txt") as ipfile:
     for line in ipfile:
         words = line.split()
-        plist.append(dict(zip(('x', 'y', 'clus'),
-                              (float(words[1]), float(words[2]), 0))))
+        plist.append((float(words[1]), float(words[2]), 0))
 
 k = input("Enter number of clusters: ")
 
-centroid = []
-
-min_x = min(plist, key=lambda x: x['x'])['x']
-max_x = max(plist, key=lambda x: x['x'])['x']
+# Calculate min, max and mean of first component for initialising centroids.
+min_x = min(plist, key=lambda x: x[0])[0]
+max_x = max(plist, key=lambda x: x[0])[0]
 mean_x = (max_x - min_x) / (k+1)
 
-min_y = min(plist, key=lambda x: x['y'])['y']
-max_y = max(plist, key=lambda x: x['y'])['y']
+# Calculate min, max and mean of second component for initialising centroids.
+min_y = min(plist, key=lambda x: x[1])[1]
+max_y = max(plist, key=lambda x: x[1])[1]
 mean_y = (max_y - min_y) / (k+1)
 
+centroid = []
+
 for i in range(1, k+1):
-    centroid.append(dict(zip(('x', 'y'),
-                    (min_x + (i * mean_x), min_y + (i * mean_y)))))
+    centroid.append((min_x + (i * mean_x), min_y + (i * mean_y)))
+
+print euclidean_dist(plist[0], plist[1])
